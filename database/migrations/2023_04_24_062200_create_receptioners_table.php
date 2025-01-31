24ae60762ab4dd458a7cc015fc7bef71333338a6
Saumy Anand <samcool3203@gmail.com>
2025-01-31 18:49:27 +0530

    admin_id added in staff, reception,request,touch with us

--- database/migrations/2023_04_24_062200_create_receptioners_table.php
+++ database/migrations/2023_04_24_062200_create_receptioners_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('receptioners', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('admin_id')->nullable();
             $table->string("name");
             $table->string("contact");
             $table->string("email");
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // $this->call(Receptioner::class);
+        $this->call(TypeSeeder::class);
+        $this->call(Receptioner::class);
+        $this->call(StaffSeeder::class);
+        $this->call(AdminSeeder::class);
+        $this->call(RequestSeeder::class);
     }
 }
