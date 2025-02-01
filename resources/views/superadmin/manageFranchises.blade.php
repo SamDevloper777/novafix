5782a17d511e6274ad1a3930b800c2756e5199e1
saurav itachi <sauravkumar52778@gmail.com>
2025-02-01 19:14:09 +0530

    search function in franchise done

--- resources/views/superadmin/manageFranchises.blade.php
+++ resources/views/superadmin/manageFranchises.blade.php
@@ -4,7 +4,7 @@
 
 
 <div class="mt-20 p-5">
-    <form class="max-w-md mx-auto mb-5">
+    <form action="{{ route('superadmin.manageFranchises') }}" class="max-w-md mx-auto mb-5">
         <label for="default-search"
             class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
         <div class="relative">
@@ -15,9 +15,9 @@
                         d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                 </svg>
             </div>
-            <input type="search" id="default-search"
+            <input type="search" name="search" id="default-search"
                 class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search State, city..." required />
+                placeholder="Search State, city..." value="{{ request('search') }}" />
             <button type="submit"
                 class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
         </div>
