1438a3e40f741359dcbf3fa2ffe2d9d8a65e17a7
Saumy Anand <samcool3203@gmail.com>
2025-02-11 15:37:27 +0530

    count added in sidebar

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -39,10 +39,20 @@
                             class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100">Add
                             Franchises</a>
                     </li>
+                    @php
+                    $franchiseCount = App\Models\Franchises::count();
+                    $RecpCount = App\Models\Receptioner::count();
+                    $staffCount = App\Models\Staff::count();
+                    @endphp
+
                     <li>
                         <a href="{{ route('admin.manageFranchises') }}"
                             class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100">Manage
-                            Franchises </a>
+                            Franchises
+                            <span class="ml-2 inline-flex items-center w-6 h-6 justify-center px-2  text-xs font-bold leading-none text-salte-600 bg-slate-200 rounded-full">
+                                {{ $franchiseCount }}
+                            </span>
+                        </a>
                     </li>
                 </ul>
             </li>
@@ -54,7 +64,10 @@
                         <path
                             d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
                     </svg>
-                    <span class="flex-1 ms-3 whitespace-nowrap">Manage Staff </span>
+                    <span class="flex-1 ms-3 whitespace-nowrap">Manage Staff
+                        <span class="ml-2 inline-flex items-center w-6 h-6 justify-center px-2 items-center  text-xs font-bold leading-none text-slate-600 bg-slate-200 rounded-full">
+                            {{ $staffCount }}
+                        </span> </span>
                 </a>
             </li>
             <li>
@@ -65,7 +78,9 @@
                         <path
                             d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
                     </svg>
-                    <span class="flex-1 ms-3 whitespace-nowrap">Manage Receptioner </span>
+                    <span class="flex-1 ms-3 whitespace-nowrap">Manage Receptioner <span class="ml-2 inline-flex items-center w-6 h-6 justify-center px-2  text-xs font-bold leading-none text-slate-700 bg-slate-200 rounded-full">
+                            {{ $RecpCount }}
+                        </span> </span>
                 </a>
             </li>
             <li>
