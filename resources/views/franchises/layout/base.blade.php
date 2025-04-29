a4a8ff8a44760945dce69f445943102b167bb593
saurav itachi <sauravkumar52778@gmail.com>
2025-04-29 14:30:51 +0530

    Update request counts in base layout to use authenticated receptionist ID

--- resources/views/franchises/layout/base.blade.php
+++ resources/views/franchises/layout/base.blade.php
@@ -38,15 +38,15 @@
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
         @php
-            
-            $NewCountReq = App\Models\Request::where('technician_id', NULL)->where('reciptionist_id')->get()->count();
-            $dateFilter = App\Models\Request::where('reciptionist_id')->get()->count();
-            $ConformCountReq = App\Models\Request::where('reciptionist_id')->where('status', 1)->get()->count();
-            $RejectedCountReq = App\Models\Request::where('reciptionist_id')->where('status', 3)->get()->count();
-            $WorkdoneCountReq = App\Models\Request::where('reciptionist_id')->where('status', 4)->get()->count();
-            $DeliveredCountReq = App\Models\Request::where('reciptionist_id')->where('status', 5)->get()->count();
-            $PendingCountReq = App\Models\Request::where('reciptionist_id')->where('status', 0)->get()->count();
-            $allReq = App\Models\Request::where('reciptionist_id')->get()->count();
+            $recept = Auth::guard('receptioner')->id();
+            $NewCountReq = App\Models\Request::where('technician_id', NULL)->where('reciptionist_id',$recept)->get()->count();
+            $dateFilter = App\Models\Request::where('reciptionist_id',$recept)->get()->count();
+            $ConformCountReq = App\Models\Request::where('reciptionist_id',$recept)->where('status', 1)->get()->count();
+            $RejectedCountReq = App\Models\Request::where('reciptionist_id',$recept)->where('status', 3)->get()->count();
+            $WorkdoneCountReq = App\Models\Request::where('reciptionist_id',$recept)->where('status', 4)->get()->count();
+            $DeliveredCountReq = App\Models\Request::where('reciptionist_id',$recept)->where('status', 5)->get()->count();
+            $PendingCountReq = App\Models\Request::where('reciptionist_id',$recept)->where('status', 0)->get()->count();
+            $allReq = App\Models\Request::where('reciptionist_id',$recept)->get()->count();
         @endphp
 
         <!-- Navbar -->
